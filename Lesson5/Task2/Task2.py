8b20d4379d06ed25bc76a0e7e8fe5c488c6491b9
P1k4sh <hsakip@gmail.com>
2022-10-07 13:45:21 +0300

    added Practice_Lesson5 files

---
+++ Lesson5/Task2/Task2.py
@@ -0,0 +1,21 @@
+from encodings import utf_8
+
+
+path = 'text.txt'
+
+dataTxt = ''
+with open(path, 'r', encoding='utf_8') as file:
+    dataTxt = file.read()
+print(dataTxt)
+
+dataTxt = dataTxt.split()
+print(dataTxt)
+
+findTxt = input('Введите текст для проверки: ')
+
+resultTxt = []
+
+for word in dataTxt:
+    if findTxt not in word:
+        resultTxt.append(word)
+print(resultTxt)
